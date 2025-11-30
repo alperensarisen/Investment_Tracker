3f0897d6413186f45dd7e2323a8d20008a27ec6e
apoliko <64897230+apoliko@users.noreply.github.com>
2025-11-30 03:26:02 +0300

    operator tests and basic bug fix

--- main.cpp
+++ main.cpp
@@ -7,11 +7,22 @@ int main(){
     stock apple("APPL",287.5);
     stock amazon("AMZN",128.3);
     stock google("GOGL",459.4);
-
+    stock tesla("TSLA",345.8);
+    cout<<"MYCART: \n";
     LinkedList myCart;
     myCart += apple;
     myCart += amazon;
     myCart += google;
     myCart.printList();
+    LinkedList myFav;
+    myFav = myCart;
+    cout<<"MYFAV:\n";
+    myFav.printList();
+    LinkedList ObList;
+    ObList += tesla;
+    cout<<"Operator + test: \n";
+    LinkedList trial;
+    trial = ObList+myFav;
+    trial.printList();
     return 0;
 }
